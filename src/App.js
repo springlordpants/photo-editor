568c686a22c8176a7be2824484c70f4b8cd93e7a
springlordpants <springlordpants@gmail.com>
2020-10-20 12:24:36 -0700

    Slider functionality complete

--- src/App.js
+++ src/App.js
@@ -82,6 +82,15 @@ function App() {
   const [options, setOptions] = useState(DEFAULT_OPTIONS)
   const selectedOption = options[selectedOptionIndex]
 
+  function handleSliderChange({target}) {
+    setOptions(prevOptions =>{
+      return prevOptions.map((option, index) => {
+        if(index !== selectedOptionIndex) return option
+        return {...option, value: target.value}
+      })
+    })
+  }
+
   return (
     <div className="container">
       <div class="main-image"></div>
@@ -97,7 +106,12 @@ function App() {
           )
         })}
       </div>
-      <Slider />
+      <Slider 
+      min={selectedOption.range.min}
+      max={selectedOption.range.max}
+      value={selectedOption.value}
+      handleChange={handleSliderChange}
+      />
     </div>
   );
 }
